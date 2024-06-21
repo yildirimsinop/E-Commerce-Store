a45ab937a13eae927868be614b273dd411c400e7
Umit Yildirim <umit.yildirim@tingtun.no>
2024-06-21 16:04:13 +0200

    database

--- API/Data/StoreContext.cs
+++ API/Data/StoreContext.cs
@@ -1,8 +1,15 @@
 
+using API.Entities;
+using Microsoft.EntityFrameworkCore;
+
 namespace API.Data
 {
-    public class StoreContext
+    public class StoreContext : DbContext
     {
+        public StoreContext(DbContextOptions options) : base(options)
+        {
+        }
 
+        public DbSet<Product> Products { get; set; }
     }
 }
