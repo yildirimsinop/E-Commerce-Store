529489c4c448410d5957c001cfb26bb9d5f555fa
Umit Yildirim <umit.yildirim@tingtun.no>
2024-06-22 21:37:09 +0200

    store.db

--- API/obj/Debug/net8.0/API.AssemblyInfo.cs
+++ API/obj/Debug/net8.0/API.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("API")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+64492e19e54620bca15451d004eb1e31631a3cdb")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+4b6567113b32c6fc722bd0bf10e73dc71a62e03c")]
 [assembly: System.Reflection.AssemblyProductAttribute("API")]
 [assembly: System.Reflection.AssemblyTitleAttribute("API")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
