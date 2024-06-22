4b6567113b32c6fc722bd0bf10e73dc71a62e03c
Umit Yildirim <umit.yildirim@tingtun.no>
2024-06-22 21:34:01 +0200

    Products

--- API/obj/Debug/net8.0/API.AssemblyInfo.cs
+++ API/obj/Debug/net8.0/API.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("API")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+1d9752e522442cd7e0735a0b80a08e654872c3d8")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+64492e19e54620bca15451d004eb1e31631a3cdb")]
 [assembly: System.Reflection.AssemblyProductAttribute("API")]
 [assembly: System.Reflection.AssemblyTitleAttribute("API")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
