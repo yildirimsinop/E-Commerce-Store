dc0fc980fa21f310256ca8b1d1dbbf92e29ff71c
Umit Yildirim <umit.yildirim@tingtun.no>
2024-06-22 21:44:05 +0200

    Sqlite

--- API/obj/Debug/net8.0/API.AssemblyInfo.cs
+++ API/obj/Debug/net8.0/API.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("API")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+4b6567113b32c6fc722bd0bf10e73dc71a62e03c")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+529489c4c448410d5957c001cfb26bb9d5f555fa")]
 [assembly: System.Reflection.AssemblyProductAttribute("API")]
 [assembly: System.Reflection.AssemblyTitleAttribute("API")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
