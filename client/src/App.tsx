de9de00a576b823251a752c39b54286cbe859620
Umit Yildirim <umit.yildirim@tingtun.no>
2024-06-24 00:09:38 +0200

    callback

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,12 +1,18 @@
-const products = [
-  { name: 'product1', price: 100.00 },
-  { name: 'product2', price: 200.00 },
-  { name: 'product2', price: 200.00 },
-]
+import { useState } from "react"
 
 function App() {
+
+  const [products, setProducts] = useState([
+    { name: 'product1', price: 100.00 },
+    { name: 'product2', price: 200.00 },
+  ]);
+
+  function addProduct() {
+    setProducts(prevState =>
+      [...prevState, { name: 'product' + (prevState.length + 1), price: (prevState.length * 100) }])
+  }
   return (
-    <div className="">
+    <div>
       <h1>Re-Store</h1>
       <ul>
         {products.map((item, index) => (
@@ -15,6 +21,7 @@ function App() {
           </li>
         ))}
       </ul>
+      <button onClick={addProduct}>Add product</button>
     </div>
   )
 }
