7757b5cf054fbc3650b20ee7ec6df3931f579c56
Umit Yildirim <umit.yildirim@tingtun.no>
2024-06-23 23:15:05 +0200

    react

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,10 +1,20 @@
+const products = [
+  { name: 'product1', price: 100.00 },
+  { name: 'product2', price: 200.00 },
+  { name: 'product2', price: 200.00 },
+]
+
 function App() {
-
-
   return (
-    <div>
+    <div className="">
       <h1>Re-Store</h1>
-
+      <ul>
+        {products.map((item, index) => (
+          <li key={index}>
+            {item.name} - {item.price}
+          </li>
+        ))}
+      </ul>
     </div>
   )
 }
