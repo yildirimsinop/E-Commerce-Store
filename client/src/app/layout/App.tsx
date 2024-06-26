f8e18d5283a55c1c559099155dfa8f00ffbf12f8
Umit Yildirim <umit.yildirim@tingtun.no>
2024-06-26 14:10:39 +0200

    mui

--- client/src/app/layout/App.tsx
+++ client/src/app/layout/App.tsx
@@ -1,5 +1,6 @@
 import { useEffect, useState } from "react"
 import { Product } from "./models/product";
+import Catalog from "../../features/catalog/Catalog";
 
 function App() {
 
@@ -25,14 +26,8 @@ function App() {
   return (
     <div>
       <h1>Re-Store</h1>
-      <ul>
-        {products.map(product => (
-          <li key={product.id}>
-            {product.name} - {product.price}
-          </li>
-        ))}
-      </ul>
-      <button onClick={addProduct}>Add products</button>
+      <Catalog products={products} addProduct={addProduct} />
+
     </div>
   )
 }
--- client/src/index.tsx
+++ client/src/index.tsx
@@ -2,6 +2,10 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './app/layout/App.tsx'
 import '../src/app/layout/styles.css'
+import '@fontsource/roboto/300.css';
+import '@fontsource/roboto/400.css';
+import '@fontsource/roboto/500.css';
+import '@fontsource/roboto/600.css';
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
