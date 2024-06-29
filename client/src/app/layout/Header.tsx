9973e0a42dcc8f72a4421dd82651b69d04779525
Umit Yildirim <umit.yildirim3068@gmail.com>
2024-06-30 01:19:32 +0200

    Header box

--- client/src/app/layout/Header.tsx
+++ client/src/app/layout/Header.tsx
@@ -64,7 +64,7 @@ export default function Header({ darkMode, handleThemeChange }: Props) {
                     )}
                 </List>
 
-                <Box >
+                <Box display='flex' alignItems='center'>
                     <IconButton size="large" edge='start' color="inherit" sx={{ mr: 2 }}>
                         <Badge badgeContent='4' color="secondary">
                             <ShoppingCart />
