8a840821bdc1517ec77664a480a1d86604851172
Umit Yildirim <umit.yildirim3068@gmail.com>
2024-06-30 20:28:22 +0200

    axios

--- client/src/features/catalog/Catalog.tsx
+++ client/src/features/catalog/Catalog.tsx
@@ -1,3 +1,4 @@
+import agent from "../../app/api/agent";
 import { Product } from "../../app/layout/models/product"
 import ProductList from "./ProductList";
 import { useState, useEffect } from "react";
@@ -7,9 +8,7 @@ export default function Catalog() {
     const [products, setProducts] = useState<Product[]>([]);
 
     useEffect(() => {
-        fetch('http://localhost:5000/api/products')
-            .then(response => response.json())
-            .then(data => setProducts(data))
+        agent.Catalog.list().then(products => setProducts(products))
     }, [])
 
     return (
