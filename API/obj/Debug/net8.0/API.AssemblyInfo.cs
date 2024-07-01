7c9b5d0237375556d955e23e510fe53a8f18c99c
Umit <yildirimsinop>
2024-07-01 12:27:34 +0200

    basket

--- API/obj/Debug/net8.0/API.AssemblyInfo.cs
+++ API/obj/Debug/net8.0/API.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("API")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+22a2792d1eac52068f1b9395a87038185309a977")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+f71337a5b276bf7ffdf04140e8acbff0847c993e")]
 [assembly: System.Reflection.AssemblyProductAttribute("API")]
 [assembly: System.Reflection.AssemblyTitleAttribute("API")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
