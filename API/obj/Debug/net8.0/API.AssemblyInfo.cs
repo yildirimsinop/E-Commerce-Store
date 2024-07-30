c269829e765b0befadcbc2bc4ac3552076c2e406
Umit <umit.yildirim3068@gmail.com>
2024-07-30 15:45:35 +0200

    api

--- API/obj/Debug/net8.0/API.AssemblyInfo.cs
+++ API/obj/Debug/net8.0/API.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("API")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+87746776f279d18d7a42b6c18b069a9de873b20d")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+31f7ae4ceb969507b89dd7d9d7a1fbbfe4333ce3")]
 [assembly: System.Reflection.AssemblyProductAttribute("API")]
 [assembly: System.Reflection.AssemblyTitleAttribute("API")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
