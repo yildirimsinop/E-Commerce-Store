e10071d86951704f5c76ba6fe0c3906c3bfc261b
Umit <umit.yildirim3068@gmail.com>
2024-08-01 18:40:40 +0200

    migration

--- API/obj/Debug/net8.0/API.AssemblyInfo.cs
+++ API/obj/Debug/net8.0/API.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("API")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+cce90e8fe10725aefd47f6d835b1751e96fa3f57")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+bcf3d1c3bea34d61121ea6e005c0ef371d778d6e")]
 [assembly: System.Reflection.AssemblyProductAttribute("API")]
 [assembly: System.Reflection.AssemblyTitleAttribute("API")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
